6a160f88253dcd0021898a44a9d7bb9878e97ea7
Rodrigo Vilar <rvilar@gmail.com>
2020-02-11 14:39:21 -0300

    Adicionando o campo Turma.anoConclusao no backend

--- examples/maiorNotaAlunos/backend/src/main/java/io/demo/potter/softwareexample/dto/TurmaDTO.java
+++ examples/maiorNotaAlunos/backend/src/main/java/io/demo/potter/softwareexample/dto/TurmaDTO.java
@@ -1,15 +1,6 @@
 package io.demo.potter.softwareexample.dto;
 
-import java.util.*;
-
-import io.demo.potter.softwareexample.model.*;
-
 import io.demo.potter.softwareexample.core.dto.ModelDTO;
-import io.demo.potter.softwareexample.enums.*;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
  * DTO for Model: Turma.
@@ -18,87 +9,96 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
  */
 public class TurmaDTO extends ModelDTO {
 
-    private Integer id;
-    
-    
-/**
-* Aluno.
-*/
-private String aluno;
+	private Integer id;
 
-/**
-* Disciplina.
-*/
-private String disciplina;
+	/**
+	 * Aluno.
+	 */
+	private String aluno;
 
-/**
-* Nota.
-*/
-private String nota;
+	/**
+	 * Disciplina.
+	 */
+	private String disciplina;
 
+	/**
+	 * Nota.
+	 */
+	private String nota;
 
-    public TurmaDTO() {}
+	private Integer anoConclusao;
 
-    public Integer getId() {
-        return this.id;
-    }
+	public TurmaDTO() {
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public Integer getId() {
+		return this.id;
+	}
 
-    /**
-* Gets the aluno.
-*
-* @return aluno.
-*/
-public String getAluno() {
-    return aluno;
-}
-/**
-* Gets the disciplina.
-*
-* @return disciplina.
-*/
-public String getDisciplina() {
-    return disciplina;
-}
-/**
-* Gets the nota.
-*
-* @return nota.
-*/
-public String getNota() {
-    return nota;
-}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    
-/**
-* Sets the aluno.
-*
-* @param name aluno.
-*/
-public void setAluno(String aluno) {
-    this.aluno = aluno;
-}
+	/**
+	 * Gets the aluno.
+	 *
+	 * @return aluno.
+	 */
+	public String getAluno() {
+		return aluno;
+	}
 
-/**
-* Sets the disciplina.
-*
-* @param name disciplina.
-*/
-public void setDisciplina(String disciplina) {
-    this.disciplina = disciplina;
-}
+	/**
+	 * Gets the disciplina.
+	 *
+	 * @return disciplina.
+	 */
+	public String getDisciplina() {
+		return disciplina;
+	}
 
-/**
-* Sets the nota.
-*
-* @param name nota.
-*/
-public void setNota(String nota) {
-    this.nota = nota;
-}
+	/**
+	 * Gets the nota.
+	 *
+	 * @return nota.
+	 */
+	public String getNota() {
+		return nota;
+	}
 
+	/**
+	 * Sets the aluno.
+	 *
+	 * @param name aluno.
+	 */
+	public void setAluno(String aluno) {
+		this.aluno = aluno;
+	}
+
+	/**
+	 * Sets the disciplina.
+	 *
+	 * @param name disciplina.
+	 */
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+
+	/**
+	 * Sets the nota.
+	 *
+	 * @param name nota.
+	 */
+	public void setNota(String nota) {
+		this.nota = nota;
+	}
+
+	public Integer getAnoConclusao() {
+		return anoConclusao;
+	}
+
+	public void setAnoConclusao(Integer anoConclusao) {
+		this.anoConclusao = anoConclusao;
+	}
 
 }
--- examples/maiorNotaAlunos/backend/src/main/java/io/demo/potter/softwareexample/model/Turma.java
+++ examples/maiorNotaAlunos/backend/src/main/java/io/demo/potter/softwareexample/model/Turma.java
@@ -1,15 +1,15 @@
 package io.demo.potter.softwareexample.model;
 
-import javax.persistence.*;
-import java.util.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import io.demo.potter.softwareexample.core.model.Model;
-import io.demo.potter.softwareexample.enums.*;
 
 /**
  * Model for table: TURMA.
@@ -21,132 +21,146 @@ import io.demo.potter.softwareexample.enums.*;
 @Where(clause = Model.WHERE_DELETED_CLAUSE)
 public class Turma extends Model<Integer> {
 
-    /**
-     * Turma ID.
-     */
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
-    private Integer id;
+	/**
+	 * Turma ID.
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Integer id;
 
-    
-/**
-* Aluno.
-*/
-@Column( name="aluno")
-private String aluno;
+	/**
+	 * Aluno.
+	 */
+	@Column(name = "aluno")
+	private String aluno;
 
-/**
-* Disciplina.
-*/
-@Column( name="disciplina")
-private String disciplina;
+	/**
+	 * Disciplina.
+	 */
+	@Column(name = "disciplina")
+	private String disciplina;
 
-/**
-* Nota.
-*/
-@Column( name="nota")
-private Double nota;
+	/**
+	 * Nota.
+	 */
+	@Column(name = "nota")
+	private Double nota;
 
+	/**
+	 * AnoConclusao.
+	 */
+	@Column(name = "anoconclusao")
+	private Integer anoConclusao;
 
-    /**
-     * If it is deleted.
-     */
-    @Column(name = "deleted")
-    private boolean deleted = false;
+	/**
+	 * If it is deleted.
+	 */
+	@Column(name = "deleted")
+	private boolean deleted = false;
 
-    public Turma() {}
+	public Turma() {
+	}
 
-    /**
-* Gets the aluno.
-*
-* @return aluno.
-*/
-public String getAluno() {
-    return aluno;
-}
-/**
-* Gets the disciplina.
-*
-* @return disciplina.
-*/
-public String getDisciplina() {
-    return disciplina;
-}
-/**
-* Gets the nota.
-*
-* @return nota.
-*/
-public Double getNota() {
-    return nota;
-}
+	/**
+	 * Gets the aluno.
+	 *
+	 * @return aluno.
+	 */
+	public String getAluno() {
+		return aluno;
+	}
 
-    
-/**
-* Sets the aluno.
-*
-* @param name aluno.
-*/
-public void setAluno(String aluno) {
-    this.aluno = aluno;
-}
+	/**
+	 * Gets the disciplina.
+	 *
+	 * @return disciplina.
+	 */
+	public String getDisciplina() {
+		return disciplina;
+	}
 
-/**
-* Sets the disciplina.
-*
-* @param name disciplina.
-*/
-public void setDisciplina(String disciplina) {
-    this.disciplina = disciplina;
-}
+	/**
+	 * Gets the nota.
+	 *
+	 * @return nota.
+	 */
+	public Double getNota() {
+		return nota;
+	}
 
-/**
-* Sets the nota.
-*
-* @param name nota.
-*/
-public void setNota(Double nota) {
-    this.nota = nota;
-}
+	/**
+	 * Sets the aluno.
+	 *
+	 * @param name aluno.
+	 */
+	public void setAluno(String aluno) {
+		this.aluno = aluno;
+	}
 
+	/**
+	 * Sets the disciplina.
+	 *
+	 * @param name disciplina.
+	 */
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
 
-    /**
-     * Gets if it is deleted.
-     * 
-     * @return If it is deleted.
-     */
-    public boolean isDeleted() {
+	/**
+	 * Sets the nota.
+	 *
+	 * @param name nota.
+	 */
+	public void setNota(Double nota) {
+		this.nota = nota;
+	}
+
+	/**
+	 * Gets if it is deleted.
+	 * 
+	 * @return If it is deleted.
+	 */
+	public boolean isDeleted() {
 		return deleted;
 	}
 
-    /**
-     * Sets if it is deleted.
-     * 
-     * @param deleted
-     * 		If it is deleted.
-     */
+	/**
+	 * Sets if it is deleted.
+	 * 
+	 * @param deleted If it is deleted.
+	 */
 	public void setDeleted(boolean deleted) {
 		this.deleted = deleted;
 	}
 
-     /**
-     * (non-Javadoc)
-     *
-     * @see Model#getId()
-     */
-    @Override
-    public Integer getId() {
-        return this.id;
-    }
+	/**
+	 * (non-Javadoc)
+	 *
+	 * @see Model#getId()
+	 */
+	@Override
+	public Integer getId() {
+		return this.id;
+	}
 
-    /**
-     * (non-Javadoc)
-     *
-     * @see Model#setId(java.io.Serializable)
-     */
-    @Override
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	/**
+	 * (non-Javadoc)
+	 *
+	 * @see Model#setId(java.io.Serializable)
+	 */
+	@Override
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getAnoConclusao() {
+		return anoConclusao;
+	}
+
+	public void setAnoConclusao(Integer anoConclusao) {
+		this.anoConclusao = anoConclusao;
+	}
+	
+	
 }
